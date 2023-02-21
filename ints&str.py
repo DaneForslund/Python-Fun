95140f264982a4af4b833291e21545daf8ede997
DaneForslund <125907506+DaneForslund@users.noreply.github.com>
2023-02-21 13:52:04 -0600

    Create ints&str.py

---
+++ ints&str.py
@@ -0,0 +1,11 @@
+# Define the release and runtime integer variables below:
+
+release_year = int("1990")
+runtime = int("3")
+measurement = str("hours") 
+print(release_year)
+print(runtime, measurement)
+rating_out_of_10 = float("5.5")
+measurement2 = str("stars")
+print(rating_out_of_10, measurement2)
+
