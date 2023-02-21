048abf2fa25d8561a61810411fc531549aff0955
DaneForslund <125907506+DaneForslund@users.noreply.github.com>
2023-02-21 13:47:53 -0600

    Create Concatenation.py

---
+++ Concatenation.py
@@ -0,0 +1,12 @@
+string1 = "Today, "
+string2 = "which began rather well, "
+string3 = "became a great challenge. "
+string4 = "I created many lines of code, "
+string5 = "because I had a big project due, "
+string6 = "but my work was eaten by Python."
+
+# Define message below:
+message = string1 + string2 + string3 + string4 + string5 + string6
+
+#print(message)
+print(message)
