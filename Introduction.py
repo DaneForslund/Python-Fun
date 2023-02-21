5a2d73f3dd99aa7c990d5fcfe98010e9bd43cb25
DaneForslund <125907506+DaneForslund@users.noreply.github.com>
2023-02-21 13:42:27 -0600

    Create Introduction.py

---
+++ Introduction.py
@@ -0,0 +1,11 @@
+my_age = 32
+half_my_age = my_age / 2
+greeting = "Hey, there! "
+name = "Dane"
+greeting_with_name = greeting + "Its good to be here with you, "
+
+print(my_age)
+print(half_my_age)
+print(greeting)
+print(name)
+print(greeting_with_name + name)
