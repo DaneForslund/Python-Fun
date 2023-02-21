63b9008a6254f3ae5d15cfb9d830b74d188d5ed7
DaneForslund <125907506+DaneForslund@users.noreply.github.com>
2023-02-21 13:40:33 -0600

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,10 @@
+#My initials are...
+
+
+print("DDDD     ccCCC    FFFFF")
+print("D    D  C     C   F   ")
+print("D    D  C         F   ")
+print("D    D  C         FFF")
+print("D    D  C         F")
+print("D    D  C     C   F")
+print("DDDD     ccCCC    F")
