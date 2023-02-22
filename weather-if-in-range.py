fc1297025135fe17fd2e4ea9da4a29b89bfeaf6e
DaneForslund <125907506+DaneForslund@users.noreply.github.com>
2023-02-22 12:03:25 -0600

    Create weather-if-in-range.py

---
+++ weather-if-in-range.py
@@ -0,0 +1,12 @@
+x = range(51)
+y = range(51, 74)
+z = range(75, 150)
+
+temp = 30
+
+if temp in x:
+  print("wear a coat")
+if temp in y:
+  print("wear whatever suits you best!")
+if temp in z:
+  print("wear a t-shirt")
